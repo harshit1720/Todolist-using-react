5480f99a84b908daad7aec0e9620200c7638be03
harshit1720 <harshittanwar02017@gmail.com>
2022-08-18 17:36:44 +0530

    first commit

---
+++ src/components/instruction.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+function instruction(){
+    return(
+        <div className = "box">
+      <h4>Instructions</h4>
+       <ul>
+        <li>Click on "Add" button to add item in the list</li>
+        <li>Click on the item to Delete from the list</li>
+       </ul>
+
+      </div>
+    )
+}
+
+export default instruction;
---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./components/App";
+import Box from "./components/instruction"
+
+ReactDOM.render(
+<div>
+<App />
+<Box />
+</div>, document.getElementById("root"));
+
